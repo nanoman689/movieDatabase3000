fd4c4ffe3d2c194d1897ee9e54ffd325dc6eeab1
Jason Thomas <nanoman689@gmail.com>
2016-06-16 20:22:43 -0700

    06-16-16_Trying to get results from actor info

--- js/app.js
+++ js/app.js
@@ -21,6 +21,11 @@ movieApp.config(function($routeProvider){
         controller: 'movieController'
     })
     
+    .when('/actor.html', {
+        templateUrl: 'pages/actor.html',
+        controller: 'actorController'
+    })
+    
 });
 
 // service
@@ -57,56 +62,21 @@ movieApp.controller('movieController',['$scope','$resource','$http', '$routePara
            })
         }
     );
-}]);
+}]);    
 
-/* 
-    $http.get("http://www.omdbapi.com/?t=" + $scope.movie + "&y=&plot=full&r=json", { 
-            $scope.details = response.data;
-            }, 
-              function(response){
-                $http.get("https://api.themoviedb.org/3/movie/" + $scope.details + "?api_key=651514f7e8896c44cfcec49d1bf2f778&find/movie/id/callback=JSON_CALLBACK", { 
-                    $scope.moreDetails = response.data; 
-                    }, 
-                        function(response) {
-        });
-    });
+/* Display the Actor info from the mouse click */
 
-
-$scope.movieDBAPI = $http.get("https://api.themoviedb.org/3/movie/" + $scope.movieIMDB + "?api_key=651514f7e8896c44cfcec49d1bf2f778&find/movie/id/callback=JSON_CALLBACK")
-        .then(function(response){
-            $scope.moreDetails = response.data;
-            
-    });
+actor.Controller('movieController',['$scope','$resource','$http', '$routeParams','movieService',function($scope, $resource, $http,  $routeParams, movieService){
     
-    console.log($scope.movieDBAPI);
+    $scope.movie = movieService.movie;
+    
+    $scope.movieAPI = $http.get("https://api.themoviedb.org/person/id/movie_credits" + $scope.details.imdbID + "?api_key=651514f7e8896c44cfcec49d1bf2f778&find/movie/id/callback=JSON_CALLBACK")
+        .then(function(response){
+            $scope.actorDetails = response.data;
+        });
+}]);    
+/*
 
-API Key: cc6412ad 
-
-Example: 
-
-http://img.omdbapi.com/?i=tt2294629&apikey=cc6412ad 
-
-
-Turn the title into it's IMBD database number to get the image.
-In this case Thor is tt0800369
-
-http://img.omdbapi.com/?i=tt0800369&apikey=cc6412ad
-
-Parameters:
-i = IMDb ID
-h (optional) = Desired height of image.
-
-    $scope.movieDBAPI = $http.get("https://api.themoviedb.org/3/movie/550?api_key=651514f7e8896c44cfcec49d1bf2f778&discover/movie?certification_country=US&certification=R&sort_by=revenue.desc&with_cast=3896?callback=JSON_CALLBACK")
-
-replace 550 with the IMDB 
-
-{{ details.imdbID }}
-
-The movie database API
-
-651514f7e8896c44cfcec49d1bf2f778
-https://api.themoviedb.org/3/movie/550?api_key=651514f7e8896c44cfcec49d1bf2f778
-
-https://api.themoviedb.org/3/movie/" + $scope.movieIMDB + "?api_key=651514f7e8896c44cfcec49d1bf2f778&find/movie/id/
+http://api.themoviedb.org/3/person/id/movie_credits
 
 */
