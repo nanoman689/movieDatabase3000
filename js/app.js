d43dc7c8b31147e728d1aa1ffbe92af84e9712e9
Jason Thomas <nanoman689@gmail.com>
2016-06-17 19:28:54 -0700

    06-17-16_Fixed the actor list

--- js/app.js
+++ js/app.js
@@ -63,18 +63,7 @@ movieApp.controller('movieController',['$scope','$resource','$http', '$routePara
         }
     );
 }]);    
-
-/* Display the Actor info from the mouse click */
-
-actor.Controller('movieController',['$scope','$resource','$http', '$routeParams','movieService',function($scope, $resource, $http,  $routeParams, movieService){
-    
-    $scope.movie = movieService.movie;
-    
-    $scope.movieAPI = $http.get("https://api.themoviedb.org/person/id/movie_credits" + $scope.details.imdbID + "?api_key=651514f7e8896c44cfcec49d1bf2f778&find/movie/id/callback=JSON_CALLBACK")
-        .then(function(response){
-            $scope.actorDetails = response.data;
-        });
-}]);    
+  
 /*
 
 http://api.themoviedb.org/3/person/id/movie_credits
