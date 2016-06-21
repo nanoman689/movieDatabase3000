717ed1fe9e9f83fb0b473693f164f6eecc66579b
Jason Thomas <nanoman689@gmail.com>
2016-06-20 19:45:28 -0700

    06-20-16_Trying_to_get_Actors_Info

--- js/app.js
+++ js/app.js
@@ -55,17 +55,75 @@ movieApp.controller('movieController',['$scope','$resource','$http', '$routePara
     $scope.movieAPI = $http.get("http://www.omdbapi.com/?t=" + $scope.movie + "&y=&plot=full&r=json")
         .then(function(response){
             $scope.details = response.data;
+        
+            console.log($scope.details.Actors);
+            
+            $scope.splitActors = function(string, nb) {
+                var array = string.split(',');
+                return array[nb];
+            }
             
             $http.get("https://api.themoviedb.org/3/movie/" + $scope.details.imdbID + "?api_key=651514f7e8896c44cfcec49d1bf2f778&find/movie/id/callback=JSON_CALLBACK")
            .then(function(response){
                $scope.moreDetails = response.data;
            })
+
         }
+
     );
+    
+    /* related movies */
+    
+    $http.get("http://www.omdbapi.com/?s=" + $scope.movie)
+        .then(function(response) {
+          $scope.related = response.data;
+    });
+    
+    /* update search on click of related movies */
+    
+    $scope.update = function(movie) {
+      $scope.movie = movieService.movie;;
+    };
+     
+    $scope.movieActor = "http://www.imdb.com/xml/find?json=1&nr=1&nm=on&q=";
+        $http({
+            method: 'jsonp',
+            url: $scope.movieActor,
+            params: {
+                format: 'jsonp',
+                q: 'Anthony Hopkins',
+                callback: 'JSON_CALLBACK'
+            }
+        }).then(function (response) {
+            alert(response.data);
+        });    
+
 }]);    
-  
+
 /*
 
+{{details.Actors}}
 http://api.themoviedb.org/3/person/id/movie_credits
+
+http://www.imdb.com/xml/find?json=1&nr=1&nm=on&q=jessica+simpson
+
+$scope.movieActor = "http://www.imdb.com/xml/find?json=1&nr=1&nm=on&q=";
+    $http({
+        method: 'jsonp',
+        url: $scope.movieActor,
+        params: {
+            format: 'jsonp',
+            q: 'Anthony Hopkins',
+            callback: 'JSON_CALLBACK'
+        }
+    }).then(function (response) {
+        alert(response.data);
+    });
+
+    $scope.movieActor = $http.get("http://www.imdb.com/xml/find?json=1&nr=1&nm=on&q=anthonyhopkins")
+        .then(function(response){
+            $scope.actorDetails = response.data;
+            console.log($scope.actorDetails);
+    });  
 
 */
